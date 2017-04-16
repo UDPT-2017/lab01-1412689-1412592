fe542472c8dff675edfabd7b9b8535e1063088a7
quynhosuperkid <hoangthibichvan95@gmail.com>
2017-04-16 21:23:54 +0700

    day la lan dau tien commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
-app.listen(process.env.PORT || 3500);
+var port     = process.env.PORT || 3500;
 
 var passport = require('passport');
 var flash    = require('connect-flash');
